a0efb1333ca48f0e1308f88af4ca313a639345a1
kxmd2008 <kxmd2008@gmail.com>
2015-02-26 21:52:26 +0800

    init

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -1,3 +1,6 @@
 create database sainteclaires;
 
 insert into account(login_name, password, type) values('admin', 'Jnw5ofpjlbE=vGiXdN246MI=', 'ADMIN');
+
+INSERT INTO sainteclaires.config (id,description,ckey,note,value,type) VALUES (17,NULL,'quarter',NULL,'1','QUARTER'
+);
